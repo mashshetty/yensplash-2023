1e9a6e6271aafe4983bcfb8d607b9bbd71f673b6
thewecas <thewecas@gmail.com>
2023-05-11 21:39:09 +0530

    fixed yensplash spelling mistake

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -11,13 +11,13 @@ function footer() {
     AOS.init({ duration: 100 })
   }, [])
   return (
-  <footer  className={styles.fcontainer}>
-      <p data-aos="fade-up"  className={styles.yenepoya}><span>YENEPOYA</span>  <span>INSTITUTE OF TECHNOLOGY</span> </p>
+    <footer className={styles.fcontainer}>
+      <p data-aos="fade-up" className={styles.yenepoya}><span>YENEPOYA</span>  <span>INSTITUTE OF TECHNOLOGY</span> </p>
       <p className={styles.subaddress}>Thodar, Mijar Post, Mangaluru Taluk -574225</p>
       <p className={styles.subaddress}> <LocalPhoneIcon className='icn' /> Tel: 08258 - 262733, 262713</p>
       <p className={styles.subaddress}><EmailIcon className='icn' /> Email: <a href="mailto:info@yit.edu.in">info@yit.edu.in</a> </p>
       <p className={styles.subaddress}><TravelExploreIcon className='icn' />Web: <a href="https://www.yit.edu.in/">www.yit.edu.in</a></p>
-      <p className={styles.copyright}>Copyright @2023 Yenspalsh. All rights reserved</p>
+      <p className={styles.copyright}>Copyright @2023 Yensplash. All rights reserved</p>
     </footer>
   )
 }
