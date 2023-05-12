2a512c4805d220c9d122bba4ece54beba93b7ce0
mashshetty <vitheshshetty555@gmail.com>
2023-05-12 20:25:24 +0530

    mash changed

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,58 +1,49 @@
-import React, { useState } from "react";
-import styles from "@/styles/Home.module.css";
 
-import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { Button } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
-import events from '../../public/events.json'
+import events from '../../public/events.json';
 
 function Eventsx() {
-  
+
   const data = 1;
   return (
-    <>
-      <div className={styles.eventContainerx}>
-     <h1 className={styles.eventHeaderx}>Events</h1>
-     </div>
     <div className={styles.eventContainerx}>
-     
-
-     {events.map((item,index)=>{
-      return( <div key={index} className={styles.eventCard}>
-        <Image
-            className={styles.eventBG}
-            src={events[index].image}
-            height="800"
-            width="800"
-            alt="event image"
-          ></Image>
-        <div  className={styles.rotatedcard}>
-          <Image
-            className={styles.eventImage}
-            src={events[index].image}
-            height="800"
-            width="800"
-            alt="event image"
-          ></Image>
-        </div>
-
-        <div className={styles.eventBottom}>
-          <h1 className={styles.eventHeader}>{events[index].name}</h1>
-          <p className={styles.eventDetails}> {events[index].schedule}</p>
-        <Link href={`events/${index}`}><Button className={styles.knowMore}>know more</Button></Link>  
-        </div>
+      <h1 className={styles.eventHeaderx}>Events</h1>
+      <div className={styles.eventContainerx}>
+        {events.map((item, index) => {
+          return (
+            <div key={index} className={styles.eventCard}>
+              <div className={styles.eventImagecard}>
+                <Image
+                  className={styles.eventImage}
+                  // src={events[index].image}
+                  src="/../public/tree.jpg"
+                  height="500"
+                  width="400"
+                  alt="event image"
+                ></Image>
+              </div>
+              <div className={styles.eventBottom}>
+                <p className={styles.eventHeader}>{events[index].name}</p>
+                <div className={styles.eventDetails}>
+                  <span><CalendarTodayIcon className={styles.icon} />&nbsp;{events[index].schedule.day}</span>
+                  <span><LocationOnIcon className={styles.icon} />&nbsp;{events[index].schedule.venue}</span>
+                  <span><AccessTimeIcon className={styles.icon} />&nbsp;{events[index].schedule.time} </span>
+                </div>
+                <Link href={`events/${index}`}><Button className={styles.knowMore}>know more</Button></Link>
+              </div>
+            </div>
+          )
+        })},
       </div>
-
-)
-     })}
-     
-
-
-
-    
     </div>
-    </>
-  );
-}
+  )
+};
+
 
 export default Eventsx;
--- src/pages/events/[eventId].js
+++ src/pages/events/[eventId].js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import styles from "@/styles/Home.module.css";
-import Image from "next/image";
-// import { event } from "./eventsList";
-import event from '../../../public/events.json'
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import event from '../../../public/events.json';
 
 function Event() {
   const router = useRouter();
@@ -11,47 +12,81 @@ function Event() {
   const [data, setData] = useState(0);
 
   useEffect(() => {
-    
+
     const { eventId } = router.query;
-    if(eventId){
-        setData(Number(eventId));
+    if (eventId) {
+      setData(Number(eventId));
     }
-  
+
   }, []);
 
 
 
   return (
-    <div className={styles.infoContainer}>
-      <div className={styles.eventDesc}>
-        <div className={styles.eventLeft}>
-          <Image
-            className={styles.eventSImg}
-            src={event[data].image}
-            width="600"
-            height="600"
-            alt="event image"
-          ></Image>
+    <div className={styles.eventPage}>
+      <img
+        className={styles.eventsImg}
+        src="/tree.jpg"
+        alt="event image"
+      ></img>
+      <div className={styles.eventName}>
+        <p>{event[data].name} </p>
+        <span>{event[data].alias} </span>
+      </div>
+      <div className={styles.eventDescription}>
+        <div className={styles.eventDetailsContainer}>
+          <div className={styles.eventDetailTitle}></div>
+          <div className={styles.eventDetailsList}>{event[data].aboutEvent}</div>
         </div>
-        <div className={styles.eventRight}>
-          <div>
-            <div className={styles.eventName}>{event[data].name}</div>
-            <div className={styles.eventDetailsx}>
-              <h3 className={styles.eventHeaders}>Description</h3>{" "}
-              <div> {event[data].description}</div>
-            </div>
-            <div className={styles.eventDetailsx}>
-              <h3 className={styles.eventHeaders}>Rules</h3>{" "}
-              <div>{event[data].rules}</div>
-            </div>
-            <div className={styles.eventDetailsx}>
-              <h3 className={styles.eventHeaders}>Co-Ordinator</h3>{" "}
-              <div>{event[data].coordinator}</div>
-            </div>
+        <div className={styles.eventDetailsContainer}>
+          <div className={styles.eventDetailTitle}>Event Details</div>
+          <div className={styles.eventDetailsList}>
+            {event[data].eventDetail.map((item, index) => (
+              <li>{item}</li>
+            ))}
           </div>
         </div>
+        <div className={styles.eventDetailsContainer}>
+          <div className={styles.eventDetailTitle}>Rules and Regulations</div>
+          <div className={styles.eventDetailsList}>
+            {event[data].rules.map((item, index) => (
+              <li>{item}</li>
+            ))}
+          </div>
+        </div>
+        <div className={styles.eventDetailsContainer}>
+          <div className={styles.eventDetailTitle}>Judging Criteria</div>
+          <div className={styles.eventDetailsList}>
+            {event[data].judgingCriteria.map((item, index) => (
+              <li>{item}</li>
+            ))}
+          </div>
+        </div>
+        <div className={styles.eventDetailsContainer}>
+          <div className={styles.eventDetailTitle}>Student Coordinators</div>
+          <div className={styles.eventDetailsList}>
+            {event[data].coordinators.students.map((item, index) => (
+              <p>{item.name} ( <a href={`tel:${item.phone}`}>{item.phone}</a>)</p>
+            ))}
+          </div>
+        </div>
+        <div className={styles.eventDetailsContainer}>
+          <div className={styles.eventDetailTitle}>Faculty Coordinators</div>
+          <div className={styles.eventDetailsList}>
+            {event[data].coordinators.faculty.map((item, index) => (
+              <p>Prof. {item}</p>
+            ))}
+          </div>
+        </div>
+
       </div>
-    </div>
+      <div className={styles.eventSchedule}>
+        <p><CalendarTodayIcon />&nbsp;&nbsp;{event[data].schedule.day}  </p>
+        <p><AccessTimeIcon />&nbsp;&nbsp;{event[data].schedule.time}  </p>
+        <p><LocationOnIcon />&nbsp;&nbsp;{event[data].schedule.venue} </p>
+      </div>
+
+    </div >
   );
 }
 
