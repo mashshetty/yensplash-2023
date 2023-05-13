7decab88a507edcc8ffb03241d5c1fa84990e57a
mashshetty <vitheshshetty555@gmail.com>
2023-05-14 00:12:57 +0530

    pro cards added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
        <Head>
-        <title>Yenspalsh 2023</title>
+       
         <meta name="description" content="yenslpash 2023" />
         <meta name="keywords" content="yensplash, yenepoya, yensplash 2023" />
         <link rel="canonical" href="https://yensplash.in" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
