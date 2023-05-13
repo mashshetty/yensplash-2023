427679f25e1064b59e5595684bce30a41ad38b1d
mashshetty <vitheshshetty555@gmail.com>
2023-05-13 23:30:06 +0530

    text changedx

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,6 +45,8 @@ export default function Home() {
       <Head>
         <title>Yenspalsh 2023</title>
         <meta name="description" content="yenslpash 2023" />
+        <meta name="keywords" content="yensplash, yenepoya, yensplash 2023" />
+        <link rel="canonical" href="https://yensplash.in" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
