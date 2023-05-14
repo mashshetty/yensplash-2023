ffac1eb6e0c68d5b6776f8abbc96495c052a2659
mashshetty <vitheshshetty555@gmail.com>
2023-05-14 15:22:39 +0530

    img added

--- src/pages/team.js
+++ src/pages/team.js
@@ -6,7 +6,8 @@ import Image from "next/image";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
-import team1 from "../../public/mash.jpg";
+
+import team1 from "../../public/vithesh.jpg";
 import team2 from "../../public/vikas.jpeg";
 import team3 from "../../public/thanay.jpeg";
 import team4 from "../../public/mehfooz.jpeg";
@@ -25,15 +26,21 @@ const Team = () => {
           </p>
           <p>Full Stack Developer</p>
           <div className={styles.social_handles}>
-            <div>
-              <FacebookIcon style={{ color: "black" }} />
+          <a href="https://www.instagram.com/1___mash___1/"> <div>
+              <InstagramIcon style={{ color: "black" }} />
+       
             </div>
+            </a>
+            <a href="https://github.com/mashshetty/">
             <div>
               <GitHubIcon style={{ color: "black" }} />
             </div>
+            </a>
+            <a href="https://www.linkedin.com/in/vithesh-shetty-091b7a222/">
             <div>
               <LinkedInIcon style={{ color: "black" }} />
             </div>
+            </a>
           </div>
         </div>
       </div>
@@ -49,7 +56,30 @@ const Team = () => {
           <p>Front-End/UI UX</p>
           <div className={styles.social_handles}>
             <div>
-              <FacebookIcon style={{ color: "black" }} />
+            <InstagramIcon style={{ color: "black" }} />
+            </div>
+            <div>
+              <GitHubIcon style={{ color: "black" }} />
+            </div>
+            <div>
+              <LinkedInIcon style={{ color: "black" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.profile_card_container}>
+        <div className={styles.profile_img}>
+          <Image width="150px" height="150px" src={team2}  alt="sponsor image"/>
+        </div>
+        <div className={styles.profile_info}>
+          <p className={styles.profile_info1}>Vishal</p>
+          <p className={styles.profile_info2}>
+            4 <sup>th</sup> YEAR CSE
+          </p>
+          <p>Front-End/UI UX</p>
+          <div className={styles.social_handles}>
+            <div>
+            <InstagramIcon style={{ color: "black" }} />
             </div>
             <div>
               <GitHubIcon style={{ color: "black" }} />
@@ -72,7 +102,7 @@ const Team = () => {
           <p>Front-End/UI UX</p>
           <div className={styles.social_handles}>
             <div>
-              <FacebookIcon style={{ color: "black" }} />
+            <InstagramIcon style={{ color: "black" }} />
             </div>
             <div>
               <GitHubIcon style={{ color: "black" }} />
@@ -95,7 +125,7 @@ const Team = () => {
           <p>Front-End/UI UX</p>
           <div className={styles.social_handles}>
             <div>
-              <FacebookIcon style={{ color: "black" }} />
+            <InstagramIcon style={{ color: "black" }} />
             </div>
             <div>
               <GitHubIcon style={{ color: "black" }} />
@@ -106,29 +136,7 @@ const Team = () => {
           </div>
         </div>
       </div>
-      <div className={styles.profile_card_container}>
-        <div className={styles.profile_img}>
-          <Image width="150px" height="150px" src={team5}  alt="sponsor image" />
-        </div>
-        <div className={styles.profile_info}>
-          <p className={styles.profile_info1}>Vishal</p>
-          <p className={styles.profile_info2}>
-            4 <sup>th</sup> YEAR CSE
-          </p>
-          <p>Front-End/UI UX</p>
-          <div className={styles.social_handles}>
-            <div>
-              <FacebookIcon style={{ color: "black" }} />
-            </div>
-            <div>
-              <GitHubIcon style={{ color: "black" }} />
-            </div>
-            <div>
-              <LinkedInIcon style={{ color: "black" }} />
-            </div>
-          </div>
-        </div>
-      </div>
+      
       <div className={styles.profile_card_container}>
         <div className={styles.profile_img}>
           <Image width="150px" height="150px" src={team1}  alt="sponsor image" />
@@ -141,7 +149,7 @@ const Team = () => {
           <p>Full-Stack</p>
           <div className={styles.social_handles}>
             <div>
-              <FacebookIcon style={{ color: "black" }} />
+            <InstagramIcon style={{ color: "black" }} />
             </div>
             <div>
               <GitHubIcon style={{ color: "black" }} />
